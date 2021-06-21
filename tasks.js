2a034759cee22f75d2e481bed9eca77f77324471
yugmani <yugmani@yahoo.com>
2021-06-20 19:17:01 -0700

    A maximal subarray

--- tasks.js
+++ tasks.js
@@ -69,7 +69,7 @@ function userInput() {
   return numArray.reduce((total, current) => total + current);
 }
 
-console.log(userInput());
+// console.log(userInput());
 
 // Alternate Solution
 // Please note the subtle, but important detail of the solution. We don’t convert value to number instantly after prompt, because after value = +value we would not be able to tell an empty string (stop sign) from the zero (valid number). We do it later instead.
@@ -96,5 +96,24 @@ function sumInput() {
 // alert( sumInput() );
 
 // **********************************
-// 2. Array Operations.
+// 5. A maximal subarray
 // **********************************
+
+// The input is an array of numbers, e.g. arr = [1, -2, 3, 4, -9, 6].
+
+// The task is: find the contiguous subarray of arr with the maximal sum of items.
+
+// Write the function getMaxSubSum(arr) that will return that sum.
+
+// For instance:
+getMaxSubSum([-1, 2, 3, -9]); // == 5 (the sum of highlighted items)
+getMaxSubSum([2, -1, 2, 3, -9]); // == 6
+getMaxSubSum([-1, 2, 3, -9, 11]); // == 11
+getMaxSubSum([-2, -1, 1, 2]); // == 3
+getMaxSubSum([100, -9, 2, -3, 5]); // == 100
+getMaxSubSum([1, 2, 3]); // == 6 (take all)
+
+// If all items are negative, it means that we take none (the subarray is empty), so the sum is zero:
+// getMaxSubSum([-1, -2, -3]) = 0
+// Please try to think of a fast solution: O(n2) or even O(n) if you can.
+
